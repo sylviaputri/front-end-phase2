75602c3a9929460ec020032535973a0fbd3a7fc5
sylviaputri <sylvi.bluemochi@gmail.com>
2019-06-05 17:14:17 +0700

    create trainee layout sidebar

--- ourproject/src/layouts/TraineeLayout.vue
+++ ourproject/src/layouts/TraineeLayout.vue
@@ -1,12 +1,45 @@
 <template>
-    <v-app id="traineeApp">
-        <h4>Layout yang nantinya untuk trainee ya </h4>
-        <p> untuk buat header dan sidebar menu trainee </p>
+    <div id="traineeApp">
+        <header>
+            <Slide>
+                <a><span>Dashboard</span></a>
+                <a><span>Semua modul</span></a>
+                <a><span>Permintaan modul</span></a>
+                <a><span>Pemintaan kelas</span></a>
+                <a><span>Akun saya</span></a>
+            </Slide>
+            <div bg-variant="light" text-variant="black" class="text-center font-weight-bold" id="headerLogo">
+                LOGOQUE
+            </div>
+        </header>
+        <br/>
+        <br/>
+        <br/>
         <router-view></router-view>
-    </v-app>
+    </div>
 </template>
 
 <script>
+import { Slide } from 'vue-burger-menu'
 export default {
+  components: {
+    Slide // Register your component
+  }
 }
 </script>
+
+<style>
+div#traineeApp header{
+    background: wheat;
+    height: 55px;
+}
+div#headerLogo{
+    font-size: 30px;
+}
+.bm-burger-button{
+    margin-top: -25px;
+}
+.bm-menu{
+    background-color: rgb(44, 76, 92)
+}
+</style>

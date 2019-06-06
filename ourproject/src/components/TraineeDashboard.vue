fc4f87260e957ef8e9160d83587e3a2fe0aff70e
sylviaputri <sylvi.bluemochi@gmail.com>
2019-06-06 17:17:34 +0700

    Update TraineeDashboard.vue

--- ourproject/src/components/TraineeDashboard.vue
+++ ourproject/src/components/TraineeDashboard.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="traineeDashboard" class="generalBackground">
+  <div id="traineeDashboard">
+      <div id="introduction" class="ml-5">
+          <h1 class="font-weight-bold">Dashboard</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+      <div id="classFollowed"></div>
+      <div id="topTrainers"></div>
+      <div id="topModul"></div>
+      <div id="modulRequested"></div>
+      <div id="classRequested"></div>
   </div>
 </template>
 

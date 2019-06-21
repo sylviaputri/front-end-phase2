c37aa73948b4a5cbb4db99c18e5adbbb44393181
sylviaputri <sylvi.bluemochi@gmail.com>
2019-06-21 10:00:19 +0700

    fix indent in modulecard.vue

--- ourproject/src/components/ModuleCard.vue
+++ ourproject/src/components/ModuleCard.vue
@@ -1,18 +1,18 @@
 <template>
     <b-card-group deck>
-          <b-card class="module mb-3 pointer" v-for="index in 5" :key="index">
-              <router-link to="/trainee/detail-module">
-                <b-card-text class="moduleRating mb-1 float-left font-weight-bold ">5.0/5.0</b-card-text>
-                <b-card-text class="moduleCategory mb-1 float-right font-weight-bold ">Artificial Intelligent</b-card-text>
-                <b-card-text class="moduleName font-weight-bold mb-1">Machine Learning V.3 <font-awesome-icon icon="file-signature" size="sm"/></b-card-text>
-                <b-card-text class=" moduleDesc mb-1 mr-5">Learn the basics of Machine Learning in this introductory course. Machine learning is transforming the world ...</b-card-text>
-                <b-card-img :src="require('./../assets/images/module_ornament.png')" class="moduleOrnament position-absolute"></b-card-img>
-                <b-card-footer class="p-0 mt-3 pr-5">
-                <b-card-text class="moduleOpenedClass float-left mb-0 grayColor">2 kelas dibuka</b-card-text>
-                <b-card-text class="moduleSession float-left mb-0 ml-4 grayColor"><font-awesome-icon icon="hourglass-half" size="sm"/> 45 menit x 3 sesi</b-card-text>
-                </b-card-footer>
-              </router-link>
-          </b-card>
+      <b-card class="module mb-3 pointer" v-for="index in 5" :key="index">
+          <router-link to="/trainee/detail-module">
+            <b-card-text class="moduleRating mb-1 float-left font-weight-bold ">5.0/5.0</b-card-text>
+            <b-card-text class="moduleCategory mb-1 float-right font-weight-bold ">Artificial Intelligent</b-card-text>
+            <b-card-text class="moduleName font-weight-bold mb-1">Machine Learning V.3 <font-awesome-icon icon="file-signature" size="sm"/></b-card-text>
+            <b-card-text class=" moduleDesc mb-1 mr-5">Learn the basics of Machine Learning in this introductory course. Machine learning is transforming the world ...</b-card-text>
+            <b-card-img :src="require('./../assets/images/module_ornament.png')" class="moduleOrnament position-absolute"></b-card-img>
+            <b-card-footer class="p-0 mt-3 pr-5">
+              <b-card-text class="moduleOpenedClass float-left mb-0 grayColor">2 kelas dibuka</b-card-text>
+              <b-card-text class="moduleSession float-left mb-0 ml-4 grayColor"><font-awesome-icon icon="hourglass-half" size="sm"/> 45 menit x 3 sesi</b-card-text>
+            </b-card-footer>
+          </router-link>
+      </b-card>
     </b-card-group>
 </template>
 

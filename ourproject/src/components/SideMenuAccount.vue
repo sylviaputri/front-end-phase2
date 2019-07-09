73bda1137deb1432fd39f53d567c85c5e73c307f
sylviaputri <sylvi.bluemochi@gmail.com>
2019-07-09 11:17:15 +0700

    Update SideMenuAccount.vue

--- ourproject/src/components/SideMenuAccount.vue
+++ ourproject/src/components/SideMenuAccount.vue
@@ -4,14 +4,14 @@
             <b-img :src="require('./../assets/images/example_person_image.jpg')" rounded="circle" class="myImg"></b-img>
             <h5 class="py-2">Nama Saya</h5>
         </div>
-        <nav class="menuAccountOption py-3 pl-3" v-if="isTrainee">
+        <nav class="menuAccountOption py-2 pl-3" v-if="isTrainee">
             <p class="pointer activeMenu">Profil</p>
             <p class="pointer">Kelas yang sedang diikuti</p>
             <p class="pointer">Permintaan modul</p>
             <p class="pointer">Permintaan kelas</p>
             <p class="pointer">Riwayat</p>
         </nav>
-        <nav class="menuAccountOption py-3 pl-3" v-if="!isTrainee">
+        <nav class="menuAccountOption py-2 pl-3" v-if="!isTrainee">
             <p class="pointer activeMenu">Profil</p>
             <p class="pointer">Rating & review tentang saya</p>
             <p class="pointer">Riwayat</p>

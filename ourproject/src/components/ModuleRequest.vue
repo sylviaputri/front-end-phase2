818e02b2f3d43e8561ccbf19d8a3eeab0d11aca9
sylviaputri <sylvi.bluemochi@gmail.com>
2019-07-30 16:42:13 +0700

    Update ModuleRequest.vue

--- ourproject/src/components/ModuleRequest.vue
+++ ourproject/src/components/ModuleRequest.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <b-card-group id="cardGroupModulRequested" class="my-3 px-2">
       <!-- example -->
       <!-- <b-card class="modulRequested my-1" v-for="index in 5" :key="index">
@@ -11,19 +12,29 @@
       </b-card> -->
       <!-- axios (undone, waktu pengajuan belum ada) -->
       <b-card class="modulRequested my-1" v-for="moduleRequest in moduleRequests" :key="moduleRequest">
-      <b-card-text class="modulRequestedName font-weight-bold mb-3">{{ moduleRequest.title }}</b-card-text>
-      <b-card-text class="modulRequestedRequester mb-2">Permintaan diajukan oleh : {{ moduleRequest.user.fullname }}</b-card-text>
-      <b-card-footer class="border-0 p-0 m-0 grayColor" style="background:transparent">
-          <b-card-text class="modulRequestedTime float-left mb-0">05-10-2019 14.03</b-card-text>
-          <b-card-text class="modulRequestedTotal float-right"><font-awesome-icon icon="thumbs-up" size="lg" class="pointer btnJoinModuleRequest"/> {{ moduleRequest.moduleRequestLikes.length }}</b-card-text>
-      </b-card-footer>
+        <b-card-text class="modulRequestedName font-weight-bold mb-3">{{ moduleRequest.title }}</b-card-text>
+        <b-card-text class="modulRequestedRequester mb-2">Permintaan diajukan oleh : {{ moduleRequest.user.fullname }}</b-card-text>
+        <b-card-footer class="border-0 p-0 m-0 grayColor" style="background:transparent">
+            <b-card-text class="modulRequestedTime float-left mb-0">05-10-2019 14.03</b-card-text>
+            <b-card-text class="modulRequestedTotal float-right"><font-awesome-icon @click="joinModuleRequest(moduleRequest.id)" icon="thumbs-up" size="lg" class="pointer btnJoinModuleRequest"/> {{ moduleRequest.moduleRequestLikes.length }}</b-card-text>
+        </b-card-footer>
       </b-card>
     </b-card-group>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['moduleRequests']
+  props: ['moduleRequests'],
+  methods: {
+    joinModuleRequest (moduleId) {
+      this.$axios.post('http://komatikugm.web.id:13370/modules/_likes', {
+        moduleId: moduleId,
+      })
+      .then(response => alert(response))
+      .catch(error => alert(error))
+    }
+  }
 }
 </script>
 

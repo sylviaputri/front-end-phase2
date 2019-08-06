bebfa50d3f31a1ebc61214d239552b456afc5ad3
sylviaputri <sylvi.bluemochi@gmail.com>
2019-08-06 23:32:49 +0700

    fixing tampilan module card

--- ourproject/src/components/ModuleCard.vue
+++ ourproject/src/components/ModuleCard.vue
@@ -1,30 +1,23 @@
 <template>
     <b-card-group deck>
-      <!-- example -->
-      <!-- <b-card class="module mb-3 pointer" v-for="index in 5" :key="index">
-          <router-link to="/trainee/detail-module">
-            <b-card-text class="moduleRating mb-1 float-left font-weight-bold ">5.0/5.0</b-card-text>
-            <b-card-text class="moduleCategory mb-1 float-right font-weight-bold ">Artificial Intelligent</b-card-text>
-            <b-card-text class="moduleName font-weight-bold mb-1">Machine Learning V.3 <font-awesome-icon icon="file-signature" size="sm"/></b-card-text>
-            <b-card-text class=" moduleDesc mb-1 mr-5">Learn the basics of Machine Learning in this introductory course. Machine learning is transforming the world ...</b-card-text>
-            <b-card-img :src="require('./../assets/images/module_ornament.png')" class="moduleOrnament position-absolute"></b-card-img>
-            <b-card-footer class="p-0 mt-3 pr-5">
-              <b-card-text class="moduleOpenedClass float-left mb-0 grayColor">2 kelas dibuka</b-card-text>
-              <b-card-text class="moduleSession float-left mb-0 ml-4 grayColor"><font-awesome-icon icon="hourglass-half" size="sm"/> 45 menit x 3 sesi</b-card-text>
-            </b-card-footer>
-          </router-link>
-      </b-card> -->
-      <!-- axios-->
       <b-card class="module mb-3 pointer" v-for="module in modules" :key="module.id">
         <router-link :to="{path: '/trainee/detail-module/' + module.id}">
-          <b-card-text class="moduleRating mb-1 float-left font-weight-bold ">{{ module.rating | ratingPrecision }} / 5.0</b-card-text>
-          <b-card-text class="moduleCategory mb-1 float-right font-weight-bold ">{{ module.category }}</b-card-text>
-          <b-card-text class="moduleName font-weight-bold mb-1">{{ module.name }} V.{{ module.version }} <font-awesome-icon v-if="module.hasExam" icon="file-signature" size="sm"/></b-card-text>
-          <b-card-text class=" moduleDesc mb-1 mr-5">{{ module.desc }}</b-card-text>
+          <b-card-body style="max-height:100px; min-height:100px">
+            <b-card-text class="moduleRating mb-1 float-left font-weight-bold ">{{ module.rating | ratingPrecision }} / 5.0</b-card-text>
+            <b-card-text class="moduleCategory mb-1 float-right font-weight-bold ">{{ module.category }}</b-card-text>
+            <b-card-text class="moduleName font-weight-bold mb-1">{{ module.name | cutModuleName }} V.{{ module.version }} <font-awesome-icon v-if="module.hasExam" icon="file-signature" size="sm"/></b-card-text>
+            <b-card-text class=" moduleDesc mb-1 mr-5" style="height:50px; overflow:hidden">{{ module.desc | cutDescription }}</b-card-text>
+          </b-card-body>
           <b-card-img :src="require('./../assets/images/module_ornament.png')" class="moduleOrnament position-absolute"></b-card-img>
           <b-card-footer class="p-0 mt-3 pr-5">
-            <b-card-text class="moduleOpenedClass float-left mb-0 grayColor">{{ module.openClassroomCount }} kelas dibuka</b-card-text>
-            <b-card-text class="moduleSession float-left mb-0 ml-4 grayColor"><font-awesome-icon icon="hourglass-half" size="sm"/> {{ module.timePerSession }} menit x {{ module.sessionCount }} sesi</b-card-text>
+            <b-row>
+              <b-col sm="4">
+                <b-card-text class="moduleOpenedClass mb-0 grayColor">{{ module.openClassroomCount }} kelas dibuka</b-card-text>
+              </b-col>
+              <b-col sm="8">
+                <b-card-text class="moduleSession mb-0 ml-4 grayColor"><font-awesome-icon icon="hourglass-half" size="sm"/> {{ module.timePerSession }} menit x {{ module.sessionCount }} sesi</b-card-text>
+              </b-col>
+            </b-row>
           </b-card-footer>
         </router-link>
       </b-card>
@@ -35,8 +28,22 @@
 export default {
   props: ['modules'],
   filters: {
-    ratingPrecision: function (value) {
+    ratingPrecision (value) {
       return value.toFixed(1)
+    },
+    cutDescription (value) {
+      if (value.length >= 72) {
+        return value.slice(0, 72) + ' ...'
+      } else {
+        return value
+      }
+    },
+    cutModuleName (value) {
+      if (value.length >= 28) {
+        return value.slice(0, 28) + ' ...'
+      } else {
+        return value
+      }
     }
   }
 }
@@ -49,6 +56,9 @@ div.module{
 }
 div.module .card-body{
   padding: 5px;
+}
+div.module .card-footer{
+  background: transparent
 }
 div.module a{
   color: black;

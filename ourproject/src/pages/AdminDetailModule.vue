83d7f04db0101176ef3710586640f6e35609783e
Erinda Resha <38478927+erindaresha@users.noreply.github.com>
2019-08-07 23:03:55 +0700

    cleaning

--- ourproject/src/pages/AdminDetailModule.vue
+++ ourproject/src/pages/AdminDetailModule.vue
@@ -26,7 +26,7 @@
             <b-col>
               <label class="mt-2">Deskripsi</label>
               <VueTrix v-model="editorContentDesc" v-if="editModule==true" placeholder="Maksimal 300 karakter"/>
-              <p v-if="editModule==false" v-html="detailModule.description"></p>
+              <p v-else v-html="detailModule.description"></p>
               <!-- <b-form-textarea disabled v-if="editModule==false" rows="8" max-rows="15" class="mb-0" v-model="detailModule.description"></b-form-textarea> -->
             </b-col>
           </b-row>
@@ -34,7 +34,7 @@
             <b-col>
               <label class="mt-2">Daftar Materi</label>
               <VueTrix v-model="editorContentList" v-if="editModule==true" placeholder="Maksimal 300 karakter"/>
-              <p v-if="editModule==false" v-html="detailModule.materialDescription"></p>
+              <p v-else v-html="detailModule.materialDescription"></p>
               <!-- <b-form-textarea disabled v-if="editModule==false" rows="8" max-rows="15" class="mb-0" v-model="detailModule.materialDescription"></b-form-textarea> -->
             </b-col>
           </b-row>
@@ -45,10 +45,11 @@
               <label class="mt-2">Status</label>
             </b-col>
             <b-col sm="9">
-              <b-form-select v-model="selected" :disabled="editModule==false">
-                <option :value="active">Aktif</option>
-                <option value="not active">Tidak Aktif</option>
+              <b-form-select v-model="selectedStatus" v-if="editModule==true">
+                <option value="open">Open</option>
+                <option value="close">Close</option>
               </b-form-select>
+              <b-form-input type="text" v-else disabled :value="selectedStatus ? 'Open' : 'Close'"></b-form-input>
             </b-col>
           </b-row>
           <b-row class="my-5">
@@ -56,9 +57,10 @@
               <label class="mt-2">Kategori</label>
             </b-col>
             <b-col sm="9">
-              <b-form-select v-model="selectedCategory" :disabled="editModule==false">
+              <b-form-select v-model="selectedCategory" v-if="editModule==true">
                 <option v-for="category in moduleCategories" :key="category.id" :value="category.id">{{category.name}}</option>
               </b-form-select>
+              <b-form-input type="text" v-else disabled v-model="detailModule.moduleCategory.name"></b-form-input>
             </b-col>
           </b-row>
           <b-row class="my-5">
@@ -66,10 +68,11 @@
               <label class="mt-2">Dengan Ujian</label>
             </b-col>
             <b-col sm="9">
-              <b-form-select v-model="selected" :disabled="editModule==false">
-                <option :value="yes">Ada</option>
-                <option value="no">Tidak</option>
+              <b-form-select v-model="selectedExam" v-if="editModule==true">
+                <option value=true>Ada</option>
+                <option value=false>Tidak</option>
               </b-form-select>
+              <b-form-input type="text" v-else disabled :value="selectedExam ? 'Ada' : 'Tidak'"></b-form-input>
             </b-col>
           </b-row>
           <b-row class="my-5">
@@ -88,7 +91,7 @@
               <b-form-input type="number" :disabled="editModule==false" v-model="detailModule.timePerSession"></b-form-input>
             </b-col>
             <b-col sm="2">
-              <label class="mt-2">Menit / Sesi</label>
+              <label class="mt-2" style="font-weight: normal;">Menit / Sesi</label>
             </b-col>
           </b-row>
         </b-col>
@@ -125,7 +128,10 @@ export default {
       editorContentDesc: null,
       editorContentList: null,
       valueDesc: null, // gak penting, post axios
-      valueList: null // gak penting, post axios
+      valueList: null, // gak penting, post axios
+      selectedCategory: null,
+      selectedExam: null,
+      selectedStatus: null
     }
   },
   methods: {
@@ -166,6 +172,9 @@ export default {
       .then(response => {
         this.detailModule = response.data.data.module
         this.editorContentDesc = response.data.data.module.description
+        this.selectedCategory = response.data.data.module.moduleCategory.id
+        this.selectedExam = response.data.data.module.hasExam
+        this.selectedStatus = response.data.data.module.status
       })
     .catch(error => { console.log(error.response) })
     this.$axios
@@ -183,5 +192,8 @@ html{
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
+}
+label{
+  font-weight: bold;
 }
 </style>

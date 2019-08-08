3d22dc99046baa0f744084815e50044c48fc694f
sylviaputri <sylvi.bluemochi@gmail.com>
2019-08-08 11:50:32 +0700

    ubah date format

--- ourproject/src/components/ModuleRequest.vue
+++ ourproject/src/components/ModuleRequest.vue
@@ -1,21 +1,11 @@
 <template>
   <div>
     <b-card-group id="cardGroupModulRequested" class="my-3 px-2">
-      <!-- example -->
-      <!-- <b-card class="modulRequested my-1" v-for="index in 5" :key="index">
-      <b-card-text class="modulRequestedName font-weight-bold mb-3">JavaScript Errors and Debugging</b-card-text>
-      <b-card-text class="modulRequestedRequester mb-2">Permintaan diajukan oleh : Nama Peminta</b-card-text>
-      <b-card-footer class="border-0 p-0 m-0 grayColor" style="background:transparent">
-          <b-card-text class="modulRequestedTime float-left mb-0">05-10-2019 14.03</b-card-text>
-          <b-card-text class="modulRequestedTotal float-right"><font-awesome-icon icon="thumbs-up" size="lg" class="pointer btnJoinModuleRequest"/> 135</b-card-text>
-      </b-card-footer>
-      </b-card> -->
-      <!-- axios (undone, waktu pengajuan belum ada) -->
       <b-card class="modulRequested my-1" v-for="moduleRequest in moduleRequests" :key="moduleRequest.moduleRequest.id">
         <b-card-text class="modulRequestedName font-weight-bold mb-3">{{ moduleRequest.moduleRequest.title }}</b-card-text>
         <b-card-text class="modulRequestedRequester mb-2">Permintaan diajukan oleh : {{ moduleRequest.moduleRequest.user.fullname }}</b-card-text>
         <b-card-footer class="border-0 p-0 m-0 grayColor" style="background:transparent">
-            <b-card-text class="modulRequestedTime float-left mb-0">{{ moduleRequest.moduleRequest.createdAt }}</b-card-text>
+            <b-card-text class="modulRequestedTime float-left mb-0">{{ moduleRequest.moduleRequest.createdAt | moment("DD-MM-YYYY hh:mm:ss") }}</b-card-text>
             <b-card-text class="modulRequestedTotal float-right"><font-awesome-icon @click="joinModuleRequest(moduleRequest.moduleRequest.id)" v-bind:class="{ lightBlueColor: moduleRequest.hasVote}" icon="thumbs-up" size="lg" class="pointer btnJoinModuleRequest"/> {{ moduleRequest.moduleRequest.moduleRequestLikes.length }}</b-card-text>
         </b-card-footer>
       </b-card>

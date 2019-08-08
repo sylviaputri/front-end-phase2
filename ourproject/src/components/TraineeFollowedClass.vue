90c215a8350fc001863c57949b4b4fbe5207cd06
sylviaputri <sylvi.bluemochi@gmail.com>
2019-08-08 22:49:48 +0700

    Update TraineeFollowedClass.vue

--- ourproject/src/components/TraineeFollowedClass.vue
+++ ourproject/src/components/TraineeFollowedClass.vue
@@ -50,7 +50,7 @@
                         <p class="classFollowedPerSession ml-5">{{ classSubscribed[0].module.timePerSession }} menit/sesi</p>
                         <light-timeline :items='classSubscribed[0].classroomSessions'>
                             <template slot='content' slot-scope='{ item }'>
-                                {{item.startTime}} <span v-if="item.exam" style="color:red">(EXAM)</span>
+                                {{item.startTime | moment("DD MMMM YYYY hh:mm:ss")}} <span v-if="item.exam" style="color:red">(EXAM)</span>
                             </template>
                         </light-timeline>
                         <!-- material -->

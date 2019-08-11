0fcdbdf8fd05427e9a50d44e5aa28d1552a4ed85
sylviaputri <sylvi.bluemochi@gmail.com>
2019-08-12 01:46:43 +0700

    display sesi berikutnya di trainer opened class

--- ourproject/src/pages/TrainerOpenedClass.vue
+++ ourproject/src/pages/TrainerOpenedClass.vue
@@ -54,7 +54,7 @@
                         <b-card-text  v-b-modal="'modal-detail-class-'+openedClass.id" class="classOpenedModuleName font-weight-old mb-0">{{ openedClass.module.name }} V.{{ openedClass.module.version }} <font-awesome-icon v-if="openedClass.module.hasExam" icon="file-signature" size="sm"/></b-card-text>
                         <b-card-text  v-b-modal="'modal-detail-class-'+openedClass.id" class="classOpenedCategory mb-2">Kategori : {{ openedClass.module.moduleCategory.name }}</b-card-text>
                         <b-button variant="primary" class="float-right py-0 mt-3" v-b-modal="'modal-close-class-'+openedClass.id">Tutup kelas</b-button>
-                        <!-- <b-card-text class="classOpenedNextSession purpleColor">Sesi berikutnya : 28 Agustus 2019</b-card-text> -->
+                        <b-card-text class="classOpenedNextSession purpleColor">Sesi berikutnya : {{ nextSession(openedClass.classroomSessions) }}</b-card-text>
                     </div>
                     <!-- Pop up -->
                     <b-modal :id="'modal-detail-class-'+openedClass.id" class="modal-detail-class" centered>
@@ -167,6 +167,15 @@ export default {
             }
         }
         return (count / classSessions.length * 100 + '%')
+    },
+    nextSession (classSessions) {
+        for (var i = 0; i < classSessions.length; i++) {
+            if (classSessions[i].startTime > new Date()) {
+                return this.$moment(classSessions[i].startTime).format('DD MMMM YYYY hh:mm')
+            } else if (i === classSessions.length - 1) {
+                return 'sudah selesai'
+            }
+        }
     }
   },
   created () {

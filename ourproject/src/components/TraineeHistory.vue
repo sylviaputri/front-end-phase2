8cc4c2383c742374d60528377a62bd22a3ee25b3
sylviaputri <sylvi.bluemochi@gmail.com>
2019-08-12 23:44:58 +0700

    sesuaikan dengan API history lulus/tidak lulus

--- ourproject/src/components/TraineeHistory.vue
+++ ourproject/src/components/TraineeHistory.vue
@@ -12,9 +12,87 @@
           </b-col>
       </b-row>
       <!-- content -->
-      <div v-if="activeTab === 1" class="fadedWhiteBackground mt-3">
-          <b-card-group id="cardGroupClassHistory" class="my-3 px-3 py-3">
-            <b-card class="classHistory my-1">
+      <div class="fadedWhiteBackground mt-3">
+          <div v-if="classesHistory === '' || classesHistory.length === 0" class="text-center py-3">
+            Tidak ada riwayat
+          </div>
+          <div v-else-if="classesHistory === null" class="text-center py-3">
+            <b-spinner label="Spinning"></b-spinner>
+          </div>
+          <b-card-group v-else id="cardGroupClassHistory" class="my-3 px-3 py-3">
+            <b-card class="classHistory my-1" v-for="classHistory in classesHistory" :key="classHistory[0].id">
+              <b-row>
+                <b-col sm="9" class="text-justify">
+                  <b-card-text class="classHistoryName my-0">{{ classHistory.classroom.name }}</b-card-text>
+                  <b-card-text class="classHistoryModuleName font-weight-bold my-0">{{ classHistory.classroom.module.name }} V.{{ classHistory.classroom.module.version }}</b-card-text>
+                  <b-card-text class="classHistoryModuleDesc">{{ classHistory.classroom.module.description }}</b-card-text>
+                </b-col>
+                <b-col sm="3" class="text-center" v-if="classHistory.classroom.module.hasExam">
+                  <b-card-text class="my-0">Nilai</b-card-text>
+                  <b-card-text class="classHistoryGrade orangeColor font-weight-bold my-0">{{ classHistory.score | scorePrecision }} / 10</b-card-text>
+                  <b-button v-b-modal="'modal-send-review-' + classHistory.id" variant="outline-dark" class="btnClassHistoryRatingReview py-1 mt-3">beri rating & review</b-button>
+                </b-col>
+                <b-col sm="3" class="text-center" v-else>
+                  <br/>
+                  <br/>
+                  <b-button v-b-modal="'modal-send-review-' + classHistory.id" variant="outline-dark" class="btnClassHistoryRatingReview py-1 mt-3">beri rating & review</b-button>
+                </b-col>
+              </b-row>
+              <!-- Pop up for send review and rating -->
+              <b-modal :id="'modal-send-review-' + classHistory.id" centered title="Beri Rating & Review">
+                <!-- rating review fot module -->
+                <p id="sendReviewModuleName" class="font-weight-bold">{{ classHistory.classroom.module.name }} V.{{ classHistory.classroom.module.version }}</p>
+                <b-row>
+                  <b-col sm="3">
+                    Rating
+                  </b-col>
+                  <b-col sm="9">
+                    <star-rating @rating-selected ="setRatingModule" v-bind:increment="0.5" v-bind:max-rating="5"
+                      inactive-color="grey" active-color="#D43300" v-bind:star-size="25" border-color="#D43300">
+                  </star-rating>
+                  </b-col>
+                </b-row>
+                <b-row class="my-4">
+                  <b-col sm="3">
+                    Review
+                  </b-col>
+                  <b-col sm="9">
+                    <b-form-textarea v-model="reviewModule" rows="4" max-rows="6"></b-form-textarea>
+                  </b-col>
+                </b-row>
+                <!-- rating review fot trainer -->
+                <p id="sendReviewModuleName" class="font-weight-bold">Pelatih : {{ classHistory.classroom.trainer.fullname }}</p>
+                <b-row>
+                  <b-col sm="3">
+                    Rating
+                  </b-col>
+                  <b-col sm="9">
+                    <star-rating @rating-selected ="setRatingTrainer" v-bind:increment="0.5" v-bind:max-rating="5"
+                      inactive-color="grey" active-color="#D43300" v-bind:star-size="25" border-color="#D43300">
+                  </star-rating>
+                  </b-col>
+                </b-row>
+                <b-row class="my-4">
+                  <b-col sm="3">
+                    Review
+                  </b-col>
+                  <b-col sm="9">
+                    <b-form-textarea v-model="reviewTrainer" rows="4" max-rows="6"></b-form-textarea>
+                  </b-col>
+                </b-row>
+                <!-- pop up footer -->
+                <template slot="modal-footer" slot-scope="{ cancel, ok }">
+                  <b-button size="sm" variant="dark" @click="cancel(); blankRatingReviewInput()" style="width:100px">
+                    Batal
+                  </b-button>
+                  <b-button size="sm" variant="primary" @click="ok(); sendRatingReviewModule(classHistory.classroom.module.id , ratingModule, reviewModule); sendRatingReviewTrainer(classHistory.classroom.trainer.id, ratingTrainer, reviewTrainer)" style="width:100px">
+                    Simpan
+                  </b-button>
+                </template>
+              </b-modal>
+            </b-card>
+            <!-- dummy data -->
+            <!-- <b-card class="classHistory my-1">
               <b-row>
                 <b-col sm="9" class="text-justify">
                   <b-card-text class="classHistoryName my-0">Kelas Visualisasi Data 1</b-card-text>
@@ -28,9 +106,7 @@
                 </b-col>
               </b-row>
             </b-card>
-            <!-- Pop up for send review and rating -->
             <b-modal id="modal-send-review-1" centered title="Beri Rating & Review">
-              <!-- rating review fot module -->
               <p id="sendReviewModuleName" class="font-weight-bold">Data Visualization with Python V.1</p>
               <b-row>
                 <b-col sm="3">
@@ -50,7 +126,6 @@
                   <b-form-textarea v-model="reviewModule" rows="4" max-rows="6"></b-form-textarea>
                 </b-col>
               </b-row>
-              <!-- rating review fot trainer -->
               <p id="sendReviewModuleName" class="font-weight-bold">Pelatih : Trainer Mantaap</p>
               <b-row>
                 <b-col sm="3">
@@ -70,7 +145,6 @@
                   <b-form-textarea v-model="reviewTrainer" rows="4" max-rows="6"></b-form-textarea>
                 </b-col>
               </b-row>
-              <!-- pop up footer -->
               <template slot="modal-footer" slot-scope="{ cancel, ok }">
                 <b-button size="sm" variant="dark" @click="cancel()" style="width:100px">
                   Batal
@@ -80,7 +154,6 @@
                 </b-button>
               </template>
             </b-modal>
-            <!-- dummy kedua -->
             <b-card class="classHistory my-1">
               <b-row>
                 <b-col sm="9" class="text-justify">
@@ -95,9 +168,7 @@
                 </b-col>
               </b-row>
             </b-card>
-            <!-- Pop up for send review and rating -->
             <b-modal id="modal-send-review-2" centered title="Beri Rating & Review">
-              <!-- rating review fot module -->
               <p id="sendReviewModuleName" class="font-weight-bold">Machine Learning Dasar V.1</p>
               <b-row>
                 <b-col sm="3">
@@ -117,7 +188,6 @@
                   <b-form-textarea v-model="reviewModule" rows="4" max-rows="6"></b-form-textarea>
                 </b-col>
               </b-row>
-              <!-- rating review fot trainer -->
               <p id="sendReviewModuleName" class="font-weight-bold">Pelatih : Trainer Mantaap</p>
               <b-row>
                 <b-col sm="3">
@@ -137,7 +207,6 @@
                   <b-form-textarea v-model="reviewTrainer" rows="4" max-rows="6"></b-form-textarea>
                 </b-col>
               </b-row>
-              <!-- pop up footer -->
               <template slot="modal-footer" slot-scope="{ cancel, ok }">
                 <b-button size="sm" variant="dark" @click="cancel()" style="width:100px">
                   Batal
@@ -146,11 +215,8 @@
                   Simpan
                 </b-button>
               </template>
-            </b-modal>
+            </b-modal> -->
         </b-card-group>
-      </div>
-      <div v-if="activeTab === 2" class="fadedWhiteBackground mt-3 py-3">
-        <p class="text-center">Tidak ada riwayat kelas</p>
       </div>
     </div>
 </template>
@@ -163,12 +229,29 @@ export default {
       ratingModule: 0,
       ratingTrainer: 0,
       reviewModule: '',
-      reviewTrainer: ''
+      reviewTrainer: '',
+      classesHistory: null
+    }
+  },
+  filters: {
+    scorePrecision (value) {
+      return value.toFixed(1)
     }
   },
   methods: {
     changeActiveTab (index) {
       this.activeTab = index
+      if (this.activeTab === 1) {
+        this.getHistoryPassed()
+      } else {
+        this.getHistoryNotPassed()
+      }
+    },
+    blankRatingReviewInput () {
+      this.ratingModule = 0
+      this.ratingTrainer = 0
+      this.reviewModule = ''
+      this.reviewTrainer = ''
     },
     setRatingModule (rating) {
       this.ratingModule = rating
@@ -181,7 +264,10 @@ export default {
           comment: reviewModule,
           value: ratingModule
       }, { withCredentials: true })
-      .then(response => (console.log(response)))
+      .then(response => {
+        (console.log(response))
+        this.blankRatingReviewInput()
+      })
       .catch(error => console.log(error))
     },
     sendRatingReviewTrainer (trainerId, ratingTrainer, reviewTrainer) {
@@ -189,11 +275,25 @@ export default {
           comment: ratingTrainer,
           value: reviewTrainer
       }, { withCredentials: true })
-      .then(response => (console.log(response)))
+      .then(response => {
+        (console.log(response))
+        this.blankRatingReviewInput()
+      })
+      .catch(error => console.log(error))
+    },
+    getHistoryPassed () {
+      this.$axios.get('http://komatikugm.web.id:13370/classrooms/_history?page=0&passed=true&size=15', { withCredentials: true })
+      .then(response => (this.classesHistory = response.data.data.content))
+      .catch(error => console.log(error))
+    },
+    getHistoryNotPassed () {
+      this.$axios.get('http://komatikugm.web.id:13370/classrooms/_history?page=0&passed=false&size=15', { withCredentials: true })
+      .then(response => (this.classesHistory = response.data.data.content))
       .catch(error => console.log(error))
     }
   },
   mounted () {
+    this.getHistoryPassed()
   }
 }
 </script>

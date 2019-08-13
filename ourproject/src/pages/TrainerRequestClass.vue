9a53bb5faa58ca09d31d70af6b0df6e5c46b4922
sylviaputri <sylvi.bluemochi@gmail.com>
2019-08-14 02:17:57 +0700

    Update TrainerRequestClass.vue
    on pagination

--- ourproject/src/pages/TrainerRequestClass.vue
+++ ourproject/src/pages/TrainerRequestClass.vue
@@ -29,7 +29,10 @@
           <div v-else-if="classRequests == null" class="text-center py-4" style="clear:both">
             <b-spinner label="Spinning"></b-spinner>
           </div>
-          <class-request v-else style="clear:both" :classRequests=classRequests></class-request>
+          <div v-else>
+            <class-request style="clear:both" :classRequests=classRequests></class-request>
+            <pagination v-if="searchKeyword === ''" :totalPages="totalPages"></pagination>
+          </div>
         </div>
       </div>
   </div>
@@ -37,17 +40,22 @@
 
 <script>
 import ClassRequest from './../components/ClassRequest.vue'
+import Pagination from './../components/Pagination.vue'
 export default {
   data () {
     return {
       isPopularActive: true,
       isNewActive: false,
       classRequests: null,
-      searchKeyword: ''
+      searchKeyword: '',
+      totalPages: 0,
+      page: 0,
+      size: 15
     }
   },
   components: {
-    'class-request': ClassRequest
+    'class-request': ClassRequest,
+    'pagination': Pagination
   },
   created () {
     window.scrollTo(0, 0)
@@ -60,19 +68,41 @@ export default {
     getClassRequests () {
       if (this.isPopularActive) {
         this.$axios
-        .get('http://komatikugm.web.id:13370/_trainer/classrooms/_requests?page=0&popular=true&size=15', {withCredentials: true})
+        .get('http://komatikugm.web.id:13370/_trainer/classrooms/_requests?page=' + this.page + '&popular=true&size=' + this.size, {withCredentials: true})
         .then(response => (this.classRequests = response.data.data.content))
         .catch(error => { console.log(error.response) })
       } else {
         this.$axios
-        .get('http://komatikugm.web.id:13370/_trainer/classrooms/_requests?page=0&popular=false&size=15', {withCredentials: true})
+        .get('http://komatikugm.web.id:13370/_trainer/classrooms/_requests?page=' + this.page + '&popular=false&size=' + this.size, {withCredentials: true})
         .then(response => (this.classRequests = response.data.data.content))
         .catch(error => { console.log(error.response) })
       }
+    },
+    getContentPage (page) {
+      this.moduleRequests = null
+      this.page = page
+      if (this.isPopularActive) {
+        this.$axios
+        .get('http://komatikugm.web.id:13370/_trainer/classrooms/_requests?page=' + this.page + '&popular=true&size=' + this.size, {withCredentials: true})
+        .then(response => (this.classRequests = response.data.data.content))
+        .catch(error => { console.log(error.response) })
+      } else {
+        this.$axios
+        .get('http://komatikugm.web.id:13370/_trainer/classrooms/_requests?page=' + this.page + '&popular=false&size=' + this.size, {withCredentials: true})
+        .then(response => (this.classRequests = response.data.data.content))
+        .catch(error => { console.log(error.response) })
+      }
+    },
+    getTotalPages () {
+      this.$axios
+        .get('http://komatikugm.web.id:13370/_trainer/classrooms/_requests?page=' + this.page + '&popular=true&size=' + this.size, {withCredentials: true})
+        .then(response => (this.totalPages = response.data.data.totalPages))
+        .catch(error => { console.log(error.response) })
     }
   },
   mounted () {
     this.getClassRequests()
+    this.getTotalPages()
   },
   watch: {
     searchKeyword () {
